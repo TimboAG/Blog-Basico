5e6e65fb85704b1b8eb69ace52a978de33e5f3d0
Alejandra Guerin <79866676+TimboAG@users.noreply.github.com>
2023-05-13 10:41:06 -0300

    personalizacion panel admin

--- proyectos/django_blog/aplicaciones/blog/admin.py
+++ proyectos/django_blog/aplicaciones/blog/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from .models import *
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
-admin.site.register(Categoria)
-admin.site.register(Autor)
+class CategoriaResorce(resources.ModelResource):
+    class Meta:
+        model = Categoria
+
+class AutorResorce(resources.ModelResource):
+    class Meta:
+        model = Autor
+
+class CategoriaAdmin(ImportExportModelAdmin,admin.ModelAdmin):
+    search_fields=['nombre',]
+    list_display=('nombre', 'estado', 'fecha_creacion',)
+    resource_class = CategoriaResorce
+    
+class AutorAdmin(ImportExportModelAdmin,admin.ModelAdmin):
+    search_fields=['nombres',  'apellidos', 'correo']
+    list_display=('nombres','apellidos','correo', 'estado', 'fecha_creacion',)
+    resource_class = AutorResorce
+admin.site.register(Categoria, CategoriaAdmin)
+admin.site.register(Autor, AutorAdmin)
--- proyectos/django_blog/django_blog/settings.py
+++ proyectos/django_blog/django_blog/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'aplicaciones.blog'
+    'aplicaciones.blog',
+    'import_export'
 ]
 
 MIDDLEWARE = [

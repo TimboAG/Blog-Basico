18c5f0c003d0d118fc504d02a85cebbd7c08a6c7
Alejandra Guerin <79866676+TimboAG@users.noreply.github.com>
2023-05-17 10:51:22 -0300

    vistas, separacion en bloques

--- proyectos/django_blog/aplicaciones/blog/views.py
+++ proyectos/django_blog/aplicaciones/blog/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "index.html")
+
+def general(request):
+    return render(request, "general.html")
+

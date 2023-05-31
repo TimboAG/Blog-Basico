1837ebcda7aca435a2113df9f735ccc54c1e31e8
Alejandra Guerin <timbocomunicacion@gmail.com>
2023-05-31 08:16:42 -0300

    barra busqueda

--- proyectos/django_blog/aplicaciones/blog/views.py
+++ proyectos/django_blog/aplicaciones/blog/views.py
@@ -1,37 +1,57 @@
 from django.shortcuts import render
 from .models import *
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
 
 # Create your views here.
 def home(request):    
     queryset= request.GET.get("buscar")
     post= Post.objects.filter(activo=True)
     if queryset:
-        post= Post.objects.filter(activo=True)
+        post= Post.objects.filter(Q(titulo__icontains = queryset ) | Q(descripcion__icontains = queryset )  ).distinct()
     return render(request, "index.html",  {"post": post})
 
 def general(request):
+    queryset= request.GET.get("buscar")
     post= Post.objects.filter(activo=True, categoria = Categoria.objects.get(nombre__iexact="General"))
+    if queryset:
+        post= Post.objects.filter(Q(titulo__icontains = queryset ) | Q(descripcion__icontains = queryset ),
+                                activo=True, categoria = Categoria.objects.get(nombre__iexact="General")).distinct()
     return render(request, "general.html",{"post": post})
 
 def cienciaficcion(request):
+    queryset= request.GET.get("buscar")    
     post= Post.objects.filter(activo=True, categoria = Categoria.objects.get(nombre__iexact="Ciencia Ficcion"))
+    if queryset:
+        post= Post.objects.filter(Q(titulo__icontains = queryset ) | Q(descripcion__icontains = queryset ),
+                                activo=True, categoria = Categoria.objects.get(nombre__iexact="Ciencia Ficcion")).distinct()
     return render(request, "cienciaficcion.html", {"post": post})
 
 def tecnologia(request):
+    queryset= request.GET.get("buscar")    
     post= Post.objects.filter(activo=True, categoria = Categoria.objects.get(nombre__iexact="Tecnologia"))
+    if queryset:
+            post= Post.objects.filter(Q(titulo__icontains = queryset ) | Q(descripcion__icontains = queryset ),
+                                activo=True, categoria = Categoria.objects.get(nombre__iexact="Tecnologia")).distinct()
     return render(request, "tecnologia.html", {"post": post})
 
 def programacion(request):
+    queryset= request.GET.get("buscar")    
     post= Post.objects.filter(activo=True, categoria = Categoria.objects.get(nombre__iexact="programacion"))
+    if queryset:
+            post= Post.objects.filter(Q(titulo__icontains = queryset ) | Q(descripcion__icontains = queryset ),
+                                activo=True, categoria = Categoria.objects.get(nombre__iexact="programacion")).distinct()
     return render(request, "programacion.html", {"post": post})
 
 def videojuegos(request):
+    queryset= request.GET.get("buscar")    
     post= Post.objects.filter(activo=True, categoria = Categoria.objects.get(nombre="Video juegos"))
+    if queryset:
+            post= Post.objects.filter(Q(titulo__icontains = queryset ) | Q(descripcion__icontains = queryset ),
+                                activo=True, categoria = Categoria.objects.get(nombre__iexact="Video juegos")).distinct()
     return render(request, "videojuegos.html", {"post": post})
 
 def postdetalle(request, slug):
     # post= Post.objects.get(slug =slug) 
     post = get_object_or_404(Post, slug =slug)
-    return render(request, "post.html", {"postdetalle": post})  
-  
+    return render(request, "post.html", {"postdetalle": post})    
